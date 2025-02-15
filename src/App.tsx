d9c22814b20b66778208f6329a14d1c8b08149d5
Rekha Tdg <rahulmanwani751@gmail.com>
2025-02-15 14:08:33 +0400

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App = () => {
     useEffect(() => {
         setLoading(true)
         const loadPosts = async () => {
-            const data = await getPosts().then(setPosts).finally(() => setLoading(false));
+            await getPosts().then(setPosts).finally(() => setLoading(false));
         };
         loadPosts();
     }, []);
